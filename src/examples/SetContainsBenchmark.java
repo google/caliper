8a724d1d0ccb73c2eb7f252d991190bd236b5a9d
kevinb@google.com <kevinb@google.com@62ec9553-1d4c-0410-be64-ad5e8ed6b23e>
2010-01-16 00:35:42 +0000

    Make SetContainsBenchmark better
    git-svn-id: https://caliper.googlecode.com/svn/trunk@76 62ec9553-1d4c-0410-be64-ad5e8ed6b23e

--- src/examples/SetContainsBenchmark.java
+++ src/examples/SetContainsBenchmark.java
@@ -20,11 +20,14 @@ import com.google.caliper.Param;
 import com.google.caliper.Runner;
 import com.google.caliper.SimpleBenchmark;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -35,96 +38,124 @@ import java.util.Set;
  * @author Kevin Bourrillion
  */
 public class SetContainsBenchmark extends SimpleBenchmark {
-  @Param private Impl impl;
+  @Param({"Hash", "Immutable"}) private Impl impl;
 
   // So far, this is the best way to test various implementations
   public enum Impl {
     Hash {
-      @Override Set<Integer> create(Collection<Integer> contents) {
-        return new HashSet<Integer>(contents);
+      @Override Set<Element> create(Collection<Element> contents) {
+        return new HashSet<Element>(contents);
       }
     },
     LinkedHash {
-      @Override Set<Integer> create(Collection<Integer> contents) {
-        return new LinkedHashSet<Integer>(contents);
+      @Override Set<Element> create(Collection<Element> contents) {
+        return new LinkedHashSet<Element>(contents);
       }
     },
     UnmodHS {
-      @Override Set<Integer> create(Collection<Integer> contents) {
-        return Collections.unmodifiableSet(new HashSet<Integer>(contents));
+      @Override Set<Element> create(Collection<Element> contents) {
+        return Collections.unmodifiableSet(new HashSet<Element>(contents));
       }
     },
     SyncHS {
-      @Override Set<Integer> create(Collection<Integer> contents) {
-        return Collections.synchronizedSet(new HashSet<Integer>(contents));
+      @Override Set<Element> create(Collection<Element> contents) {
+        return Collections.synchronizedSet(new HashSet<Element>(contents));
       }
     },
 
     // Kind of cheating here -- Caliper just happens to bundle Google Collections so I'm testing
     // this from it; this might not work at the command line since GC are jarjar'd for caliper.jar
     Immutable {
-      @Override Set<Integer> create(Collection<Integer> contents) {
+      @Override Set<Element> create(Collection<Element> contents) {
         return ImmutableSet.copyOf(contents);
       }
     };
 
-    abstract Set<Integer> create(Collection<Integer> contents);
+    abstract Set<Element> create(Collection<Element> contents);
   }
 
   @Param private int size;
   public static final Collection<Integer> sizeValues = Arrays.asList(
-      (1<<2) - 1,
-      (1<<2),
+      (1<<3) - 1, // 7
+      (1<<3),
       (1<<6) - 1,
       (1<<6),
       (1<<10) - 1,
       (1<<10),
-      (1<<14) - 1,
-      (1<<14),
-      (1<<18) - 1,
-      (1<<18)
+      (1<<15) - 1,
+      (1<<15)
   );
+
+  @Param({"0.2", "0.8"}) private double hitRate;
+
+  @Param({"true", "false"}) // TODO: that should be assumed
+  private boolean isUserTypeFast;
 
   // "" means no fixed seed
   @Param("") private SpecialRandom random;
 
   // the following must be set during setUp
-  private Integer[] queries;
-  private Set<Integer> setToTest;
+  private Element[] queries;
+  private Set<Element> setToTest;
 
-  // Queries are just sequential integers. Since the contents of the set were
-  // chosen randomly, this shouldn't cause any undue bias.
   @Override public void setUp() {
-    this.queries = new Integer[size * 2];
-    for (int i = 0; i < size * 2; i++) {
-      queries[i] = i;
-    }
-    Collections.shuffle(Arrays.asList(queries), random);
-
-    setToTest = impl.create(createData());
+    Set<Element> valuesInSet = createData();
+    this.queries = createQueries(valuesInSet, 1024);
+    this.setToTest = impl.create(valuesInSet);
   }
 
-  private Collection<Integer> createData() {
-    Set<Integer> tempSet = new HashSet<Integer>(size * 3 / 2);
+  private Element[] createQueries(Set<Element> elementsInSet, int numQueries) {
+    List<Element> queryList = Lists.newArrayListWithCapacity(numQueries);
 
-    // Choose 50% of the numbers between 0 and max to be in the set; thus we
-    // are measuring performance of contains() when there is a 50% hit rate
-    int max = size * 2;
-    while (tempSet.size() < size) {
-      tempSet.add(random.nextInt(max));
+    int numGoodQueries = (int) (numQueries * hitRate + 0.5);
+
+    // add good queries
+    int size = elementsInSet.size();
+    int minCopiesOfEachGoodQuery = numGoodQueries / size;
+    int extras = numGoodQueries % size;
+
+    for (int i = 0; i < minCopiesOfEachGoodQuery; i++) {
+      queryList.addAll(elementsInSet);
     }
-    return tempSet;
+    List<Element> tmp = Lists.newArrayList(elementsInSet);
+    Collections.shuffle(tmp, random);
+    queryList.addAll(tmp.subList(0, extras));
+
+    // now add bad queries
+    while (queryList.size() < numQueries) {
+      Element candidate = newElement();
+      if (!elementsInSet.contains(candidate)) {
+        queryList.add(candidate);
+      }
+    }
+    Collections.shuffle(queryList, random);
+    return queryList.toArray(new Element[0]);
+  }
+
+  private Set<Element> createData() {
+    Set<Element> set = Sets.newHashSetWithExpectedSize(size);
+    while (set.size() < size) {
+      set.add(newElement());
+    }
+    return set;
+  }
+
+  private Element newElement() {
+    int value = random.nextInt();
+    return isUserTypeFast
+        ? new Element(value)
+        : new SlowElement(value);
   }
 
   public boolean timeContains(int reps) {
     // Paranoia: acting on hearsay that accessing fields might be slow
     // Should write a benchmark to test that!
-    Set<Integer> set = setToTest;
-    Integer[] queries = this.queries;
+    Set<Element> set = setToTest;
+    Element[] queries = this.queries;
 
     // Allows us to use & instead of %, acting on hearsay that division operators (/%) are
     // disproportionately expensive; should test this too!
-    int mask = Integer.highestOneBit(size * 2) - 1;
+    int mask = queries.length - 1;
 
     boolean dummy = false;
     for (int i = 0; i < reps; i++) {
@@ -138,6 +169,44 @@ public class SetContainsBenchmark extends SimpleBenchmark {
     Runner.main(SetContainsBenchmark.class, args);
   }
 
+  private static class Element implements Comparable<Element> {
+    final int hash;
+    Element(int hash) {
+      this.hash = hash;
+    }
+    @Override public boolean equals(Object obj) {
+      return this == obj
+          || (obj instanceof Element && ((Element) obj).hash == hash);
+    }
+    @Override public int hashCode() {
+      return hash;
+    }
+    public int compareTo(Element that) {
+      return (hash < that.hash) ? -1 : (hash > that.hash) ? 1 : 0;
+    }
+    @Override public String toString() {
+      return String.valueOf(hash);
+    }
+  }
+
+  private static class SlowElement extends Element {
+    private SlowElement(int hash) {
+      super(hash);
+    }
+    @Override public boolean equals(Object obj) {
+      return slowItDown() != 1 && super.equals(obj);
+    }
+    @Override public int hashCode() {
+      return slowItDown() + hash;
+    }
+    static int slowItDown() {
+      int result = 0;
+      for (int i = 1; i <= 1000; i++) {
+        result += i;
+      }
+      return result;
+    }
+  }
 
   // Just an experiment with a slightly nicer way to create Randoms for benchies
 
