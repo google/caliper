649b358c44947ebf902b77275befc851d38b4cde
jsharpe@google.com <jsharpe@google.com@62ec9553-1d4c-0410-be64-ad5e8ed6b23e>
2010-07-12 18:55:16 +0000

    Cleaned up xml generation and parsing to reduce duplication and added some comments showing the schema
    git-svn-id: https://caliper.googlecode.com/svn/trunk@144 62ec9553-1d4c-0410-be64-ad5e8ed6b23e

--- src/com/google/caliper/Xml.java
+++ src/com/google/caliper/Xml.java
@@ -36,42 +36,109 @@ import org.w3c.dom.NodeList;
 
 public final class Xml {
   private static final String DATE_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ssz";
+  private static final String ENVIRONMENT_ELEMENT_NAME = "environment";
+  private static final String RESULT_ELEMENT_NAME = "result";
+  private static final String RUN_ELEMENT_NAME = "run";
+  private static final String BENCHMARK_ATTRIBUTE = "benchmark";
+  private static final String API_KEY_ATTRIBUTE = "apiKey";
+  private static final String EXECUTED_TIMESTAMP_ATTRIBUTE = "executedTimestamp";
+  private static final String SCENARIO_ELEMENT_NAME = "scenario";
+
+  private static Element createResultElement(Document doc, Result result) {
+    Element topElement = doc.createElement(RESULT_ELEMENT_NAME);
+    topElement.appendChild(createEnvironmentElement(doc, result.getEnvironment()));
+    topElement.appendChild(createRunElement(doc, result.getRun()));
+    return topElement;
+  }
+
+  private static Result readResultElement(Element element) throws Exception {
+    Environment environment = null;
+    Run run = null;
+    for (Node topLevelNode : childrenOf(element)) {
+      if (topLevelNode.getNodeName().equals(ENVIRONMENT_ELEMENT_NAME)) {
+        Element environmentElement = (Element) topLevelNode;
+        environment = readEnvironmentElement(environmentElement);
+      } else if (topLevelNode.getNodeName().equals(RUN_ELEMENT_NAME)) {
+        run = readRunElement((Element) topLevelNode);
+      } else {
+        throw new RuntimeException("illegal node name: " + topLevelNode.getNodeName());
+      }
+    }
+
+    if (environment == null || run == null) {
+      throw new RuntimeException("missing environment or run elements");
+    }
+
+    return new Result(run, environment);
+  }
+
+  private static Element createEnvironmentElement(Document doc, Environment environment) {
+    Element environmentElement = doc.createElement(ENVIRONMENT_ELEMENT_NAME);
+    for (Map.Entry<String, String> entry : environment.getProperties().entrySet()) {
+      environmentElement.setAttribute(entry.getKey(), entry.getValue());
+    }
+    return environmentElement;
+  }
+
+  private static Environment readEnvironmentElement(Element element) {
+    return new Environment(attributesOf(element));
+  }
+
+  private static Element createRunElement(Document doc, Run run) {
+    Element runElement = doc.createElement(RUN_ELEMENT_NAME);
+    runElement.setAttribute(BENCHMARK_ATTRIBUTE, run.getBenchmarkName());
+    runElement.setAttribute(API_KEY_ATTRIBUTE, run.getApiKey());
+    String executedTimestampString = new SimpleDateFormat(DATE_FORMAT_STRING)
+        .format(run.getExecutedTimestamp());
+    runElement.setAttribute(EXECUTED_TIMESTAMP_ATTRIBUTE, executedTimestampString);
+
+    for (Map.Entry<Scenario, MeasurementSet> entry : run.getMeasurements().entrySet()) {
+      Element scenarioElement = doc.createElement(SCENARIO_ELEMENT_NAME);
+      runElement.appendChild(scenarioElement);
+
+      Scenario scenario = entry.getKey();
+      for (Map.Entry<String, String> parameter : scenario.getVariables().entrySet()) {
+        scenarioElement.setAttribute(parameter.getKey(), parameter.getValue());
+      }
+      scenarioElement.setTextContent(String.valueOf(entry.getValue()));
+    }
+    return runElement;
+  }
+
+  private static Run readRunElement(Element element) throws Exception {
+    String benchmarkName = element.getAttribute(BENCHMARK_ATTRIBUTE);
+    String apiKey = element.getAttribute(API_KEY_ATTRIBUTE);
+    String executedDateString = element.getAttribute(EXECUTED_TIMESTAMP_ATTRIBUTE);
+    Date executedDate = new SimpleDateFormat(DATE_FORMAT_STRING).parse(executedDateString);
+
+    ImmutableMap.Builder<Scenario, MeasurementSet> measurementsBuilder = ImmutableMap.builder();
+    for (Node node : childrenOf(element)) {
+      Element scenarioElement = (Element) node;
+      Scenario scenario = new Scenario(attributesOf(scenarioElement));
+      MeasurementSet measurement = MeasurementSet.valueOf(scenarioElement.getTextContent());
+      measurementsBuilder.put(scenario, measurement);
+    }
+
+    return new Run(measurementsBuilder.build(), benchmarkName, apiKey, executedDate);
+  }
 
   /**
-   * Encodes this result as XML to the specified stream. This XML can be parsed
+   * Encodes this run as XML to the specified stream. This XML can be parsed
    * with {@link #runFromXml(InputStream)}. Sample output:
    * <pre>{@code
-   * <result benchmark="examples.FooBenchmark"
-   *     apiKey="56b35ad1-2985-4541-8f40-170471a46693"
-   *     executedTimestamp="2010-01-05T11:08:15PST">
+   * <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+   * <run apiKey="56b35ad1-2985-4541-8f40-170471a46693"
+   *      benchmark="examples.FooBenchmark"
+   *      executedTimestamp="2010-07-12T11:38:47PDT">
    *   <scenario bar="15" foo="A" vm="dalvikvm">1200.1</scenario>
    *   <scenario bar="15" foo="B" vm="dalvikvm">1100.2</scenario>
-   * </result>
+   * </run>
    * }</pre>
    */
   public static void runToXml(Run run, OutputStream out) {
     try {
       Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-      Element result = doc.createElement("result");
-      doc.appendChild(result);
-
-      result.setAttribute("benchmark", run.getBenchmarkName());
-      result.setAttribute("apiKey", run.getApiKey());
-      String executedTimestampString = new SimpleDateFormat(DATE_FORMAT_STRING)
-          .format(run.getExecutedTimestamp());
-      result.setAttribute("executedTimestamp", executedTimestampString);
-
-      for (Map.Entry<Scenario, MeasurementSet> entry : run.getMeasurements().entrySet()) {
-        Element runElement = doc.createElement("scenario");
-        result.appendChild(runElement);
-
-        Scenario scenario = entry.getKey();
-        for (Map.Entry<String, String> parameter : scenario.getVariables().entrySet()) {
-          runElement.setAttribute(parameter.getKey(), parameter.getValue());
-        }
-        runElement.setTextContent(String.valueOf(entry.getValue()));
-      }
-
+      doc.appendChild(createRunElement(doc, run));
       TransformerFactory.newInstance().newTransformer()
           .transform(new DOMSource(doc), new StreamResult(out));
     } catch (Exception e) {
@@ -79,14 +146,21 @@ public final class Xml {
     }
   }
 
+  /**
+   * Encodes this environment as XML to the specified stream. This XML can be parsed
+   * with {@link #environmentFromXml(InputStream)}. Sample output:
+   * <pre>{@code
+   * <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+   * <environment host.cpu.cachesize="[8192 KB x 4]" host.cpu.cores="[4 x 4]"
+   *              ...
+   *              jre.vmname="Java HotSpot(TM) 64-Bit Server VM" jre.vmversion="16.3-b01"
+   *              os.arch="amd64" os.name="Linux" os.version="2.6.24-gg804011-generic"/>
+   * }</pre>
+   */
   public static void environmentToXml(Environment environment, OutputStream out) {
     try {
       Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-      Element environmentElement = doc.createElement("environment");
-      doc.appendChild(environmentElement);
-      for (Map.Entry<String, String> entry : environment.getProperties().entrySet()) {
-        environmentElement.setAttribute(entry.getKey(), entry.getValue());
-      }
+      doc.appendChild(createEnvironmentElement(doc, environment));
       TransformerFactory.newInstance().newTransformer()
           .transform(new DOMSource(doc), new StreamResult(out));
     } catch (Exception e) {
@@ -94,42 +168,29 @@ public final class Xml {
     }
   }
 
+  /**
+   * Encodes this result as XML to the specified stream. This XML can be parsed
+   * with {@link #resultFromXml(InputStream)}. Sample output:
+   * <pre>{@code
+   * <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+   * <result>
+   *     <environment host.cpu.cachesize="[8192 KB x 4]" host.cpu.cores="[4 x 4]"
+   *                  ...
+   *                  jre.vmname="Java HotSpot(TM) 64-Bit Server VM" jre.vmversion="16.3-b01"
+   *                  os.arch="amd64" os.name="Linux" os.version="2.6.24-gg804011-generic"/>
+   *     <run apiKey="56b35ad1-2985-4541-8f40-170471a46693"
+   *          benchmark="examples.FooBenchmark"
+   *          executedTimestamp="2010-07-12T11:38:47PDT">
+   *       <scenario bar="15" foo="A" vm="dalvikvm">1200.1</scenario>
+   *       <scenario bar="15" foo="B" vm="dalvikvm">1100.2</scenario>
+   *     </run>
+   * </result>
+   * }</pre>
+   */
   public static void resultToXml(Result result, OutputStream out) {
-    Run run = result.getRun();
-    Environment environment = result.getEnvironment();
     try {
       Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-
-      Element topElement = doc.createElement("result");
-      doc.appendChild(topElement);
-
-      Element environmentElement = doc.createElement("environment");
-      topElement.appendChild(environmentElement);
-
-      for (Map.Entry<String, String> entry : environment.getProperties().entrySet()) {
-        environmentElement.setAttribute(entry.getKey(), entry.getValue());
-      }
-
-      Element resultElement = doc.createElement("run");
-      topElement.appendChild(resultElement);
-
-      resultElement.setAttribute("benchmark", run.getBenchmarkName());
-      resultElement.setAttribute("apiKey", run.getApiKey());
-      String executedTimestampString = new SimpleDateFormat(DATE_FORMAT_STRING)
-          .format(run.getExecutedTimestamp());
-      resultElement.setAttribute("executedTimestamp", executedTimestampString);
-
-      for (Map.Entry<Scenario, MeasurementSet> entry : run.getMeasurements().entrySet()) {
-        Element runElement = doc.createElement("scenario");
-        resultElement.appendChild(runElement);
-
-        Scenario scenario = entry.getKey();
-        for (Map.Entry<String, String> parameter : scenario.getVariables().entrySet()) {
-          runElement.setAttribute(parameter.getKey(), parameter.getValue());
-        }
-        runElement.setTextContent(String.valueOf(entry.getValue()));
-      }
-
+      doc.appendChild(createResultElement(doc, result));
       TransformerFactory.newInstance().newTransformer()
           .transform(new DOMSource(doc), new StreamResult(out));
     } catch (Exception e) {
@@ -144,22 +205,7 @@ public final class Xml {
   public static Run runFromXml(InputStream in) {
     try {
       Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
-      Element result = document.getDocumentElement();
-
-      String benchmarkName = result.getAttribute("benchmark");
-      String apiKey = result.getAttribute("apiKey");
-      String executedDateString = result.getAttribute("executedTimestamp");
-      Date executedDate = new SimpleDateFormat(DATE_FORMAT_STRING).parse(executedDateString);
-
-      ImmutableMap.Builder<Scenario, MeasurementSet> measurementsBuilder = ImmutableMap.builder();
-      for (Node node : childrenOf(result)) {
-        Element scenarioElement = (Element) node;
-        Scenario scenario = new Scenario(attributesOf(scenarioElement));
-        MeasurementSet measurement = MeasurementSet.valueOf(scenarioElement.getTextContent());
-        measurementsBuilder.put(scenario, measurement);
-      }
-
-      return new Run(measurementsBuilder.build(), benchmarkName, apiKey, executedDate);
+      return readRunElement(document.getDocumentElement());
     } catch (Exception e) {
       throw new IllegalStateException("Malformed XML document", e);
     }
@@ -173,7 +219,7 @@ public final class Xml {
     try {
       Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
       Element environmentElement = document.getDocumentElement();
-      return new Environment(attributesOf(environmentElement));
+      return readEnvironmentElement(environmentElement);
     } catch (Exception e) {
       throw new IllegalStateException("Malformed XML document", e);
     }
@@ -182,41 +228,7 @@ public final class Xml {
   public static Result resultFromXml(InputStream in) {
     try {
       Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
-
-      Element resultElement = document.getDocumentElement();
-      Environment environment = null;
-      Run run = null;
-      for (Node topLevelNode : childrenOf(resultElement)) {
-        if (topLevelNode.getNodeName().equals("environment")) {
-          Element environmentNode = (Element) topLevelNode;
-          environment = new Environment(attributesOf(environmentNode));
-        } else if (topLevelNode.getNodeName().equals("run")) {
-          Element result = (Element) topLevelNode;
-
-          String benchmarkName = result.getAttribute("benchmark");
-          String apiKey = result.getAttribute("apiKey");
-          String executedDateString = result.getAttribute("executedTimestamp");
-          Date executedDate = new SimpleDateFormat(DATE_FORMAT_STRING).parse(executedDateString);
-
-          ImmutableMap.Builder<Scenario, MeasurementSet> measurementsBuilder = ImmutableMap.builder();
-          for (Node node : childrenOf(result)) {
-            Element scenarioElement = (Element) node;
-            Scenario scenario = new Scenario(attributesOf(scenarioElement));
-            MeasurementSet measurement = MeasurementSet.valueOf(scenarioElement.getTextContent());
-            measurementsBuilder.put(scenario, measurement);
-          }
-
-          run = new Run(measurementsBuilder.build(), benchmarkName, apiKey, executedDate);
-        } else {
-          throw new RuntimeException("illegal node name: " + topLevelNode.getNodeName());
-        }
-      }
-
-      if (environment == null || run == null) {
-        throw new RuntimeException("missing environment or run elements");
-      }
-
-      return new Result(run, environment);
+      return readResultElement(document.getDocumentElement());
     } catch (Exception e) {
       throw new IllegalStateException("Malformed XML document", e);
     }
