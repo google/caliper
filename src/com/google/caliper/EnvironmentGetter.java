271e2728ae3a416dab4d56f81fb00a1c189123e2
jessewilson@google.com <jessewilson@google.com@62ec9553-1d4c-0410-be64-ad5e8ed6b23e>
2010-10-23 17:35:26 +0000

    Teach Caliper how to inspect an Android device for its environment.
    git-svn-id: https://caliper.googlecode.com/svn/trunk@248 62ec9553-1d4c-0410-be64-ad5e8ed6b23e

--- src/com/google/caliper/EnvironmentGetter.java
+++ src/com/google/caliper/EnvironmentGetter.java
@@ -28,6 +28,8 @@ import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class EnvironmentGetter {
 
@@ -60,21 +62,46 @@ public final class EnvironmentGetter {
     } catch (UnknownHostException ignored) {
     }
 
-    if (osName.equals("Linux")) { // the following probably doesn't work on ALL linux
-      Multimap<String, String> cpuInfo = propertiesFromLinuxFile("/proc/cpuinfo");
-      propertyMap.put("host.cpus", Integer.toString(cpuInfo.get("processor").size()));
-      String s = "cpu cores";
-      propertyMap.put("host.cpu.cores", describe(cpuInfo, s));
-      propertyMap.put("host.cpu.names", describe(cpuInfo, "model name"));
-      propertyMap.put("host.cpu.cachesize", describe(cpuInfo, "cache size"));
-
-      Multimap<String, String> memInfo = propertiesFromLinuxFile("/proc/meminfo");
-      // TODO redo memInfo.toString() so we don't get square brackets
-      propertyMap.put("host.memory.physical", memInfo.get("MemTotal").toString());
-      propertyMap.put("host.memory.swap", memInfo.get("SwapTotal").toString());
+    if (osName.equals("Linux")) {
+      getLinuxEnvironment(propertyMap);
     }
 
     return new Environment(propertyMap);
+  }
+
+  private void getLinuxEnvironment(Map<String, String> propertyMap) {
+    // the following probably doesn't work on ALL linux
+    Multimap<String, String> cpuInfo = propertiesFromLinuxFile("/proc/cpuinfo");
+    propertyMap.put("host.cpus", Integer.toString(cpuInfo.get("processor").size()));
+    String s = "cpu cores";
+    propertyMap.put("host.cpu.cores", describe(cpuInfo, s));
+    propertyMap.put("host.cpu.names", describe(cpuInfo, "model name"));
+    propertyMap.put("host.cpu.cachesize", describe(cpuInfo, "cache size"));
+
+    Multimap<String, String> memInfo = propertiesFromLinuxFile("/proc/meminfo");
+    // TODO redo memInfo.toString() so we don't get square brackets
+    propertyMap.put("host.memory.physical", memInfo.get("MemTotal").toString());
+    propertyMap.put("host.memory.swap", memInfo.get("SwapTotal").toString());
+
+    getAndroidEnvironment(propertyMap);
+  }
+
+  private void getAndroidEnvironment(Map<String, String> propertyMap) {
+    try {
+      Map<String, String> map = getAndroidProperties();
+      String manufacturer = map.get("ro.product.manufacturer");
+      String device = map.get("ro.product.device");
+      propertyMap.put("android.device", manufacturer + " " + device); // "Motorola sholes"
+
+      String brand = map.get("ro.product.brand");
+      String model = map.get("ro.product.model");
+      propertyMap.put("android.model", brand + " " + model); // "verizon Droid"
+
+      String release = map.get("ro.build.version.release");
+      String id = map.get("ro.build.id");
+      propertyMap.put("android.release", release + " " + id); // "Gingerbread GRH07B"
+    } catch (IOException ignored) {
+    }
   }
 
   private static String describe(Multimap<String, String> cpuInfo, String s) {
@@ -114,5 +141,36 @@ public final class EnvironmentGetter {
     } catch (IOException e) {
       return ImmutableMultimap.of();
     }
+  }
+
+  public static void main(String[] args) {
+    Environment snapshot = new EnvironmentGetter().getEnvironmentSnapshot();
+    for (Map.Entry<String, String> entry : snapshot.getProperties().entrySet()) {
+      System.out.println(entry.getKey() + " " + entry.getValue());
+    }
+  }
+
+  /**
+   * Android properties are available from adb shell /system/bin/getprop. That
+   * program prints Android system properties in this format:
+   * [ro.product.model]: [Droid]
+   * [ro.product.brand]: [verizon]
+   */
+  private static Map<String, String> getAndroidProperties() throws IOException {
+    Map<String, String> result = new HashMap<String, String>();
+
+    Process process = Runtime.getRuntime().exec(new String[] {"/system/bin/getprop"});
+    BufferedReader reader = new BufferedReader(
+        new InputStreamReader(process.getInputStream(), "ISO-8859-1"));
+
+    Pattern pattern = Pattern.compile("\\[([^\\]]*)\\]: \\[([^\\]]*)\\]");
+    String line;
+    while ((line = reader.readLine()) != null) {
+      Matcher matcher = pattern.matcher(line);
+      if (matcher.matches()) {
+        result.put(matcher.group(1), matcher.group(2));
+      }
+    }
+    return result;
   }
 }
